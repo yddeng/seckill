4d2000d21b24d3185478d6abc8a8818c46091b77
yidongdeng <248244142@qq.com>
2021-02-04 17:25:48 +0800

    none

--- main/seckill.go
+++ main/seckill.go
@@ -1,9 +1,14 @@
 package main
 
-import "github.com/yddeng/seckill"
+import (
+	"github.com/yddeng/seckill"
+	"log"
+)
 
 func main() {
 	seckill.LoadConfig("./config.toml")
 
 	seckill.Seckill()
+
+	log.Println("Seckill Stop")
 }
